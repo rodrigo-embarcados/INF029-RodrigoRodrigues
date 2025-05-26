8b06b67fcd1072dd26a9e32b16d30498eeb0264f
rodrigo-embarcados <rodrigo.rdvr@gmail.com>
2025-05-26 00:07:44 -0300

    Resoluções

--- Trabalho 1/RodrigoRodrigues20232160034.c
+++ Trabalho 1/RodrigoRodrigues20232160034.c
@@ -83,44 +83,44 @@ char tira_acento_letra(uint8_t byte_ms)
 {
   switch (byte_ms)
   {
-  case 0xA1:
-  case 0xA0:
-  case 0xA2:
-  case 0xA3:
-    return 'a'; // á à â ã
-  case 0xA9:
-  case 0xAA:
-    return 'e'; // é ê
-  case 0xAD:
-    return 'i'; // í
-  case 0xB3:
-  case 0xB4:
-  case 0xB5:
-    return 'o'; // ó ô õ
-  case 0xBA:
-  case 0xBC:
-    return 'u'; // ú ü
-  case 0xA7:
-    return 'c'; // ç
-  case 0x81:
-  case 0x80:
-  case 0x82:
-  case 0x83:
-    return 'A'; // Á À Â Ã
-  case 0x89:
-  case 0x8A:
-    return 'E'; // É Ê
-  case 0x8D:
-    return 'I'; // Í
-  case 0x93:
-  case 0x94:
-  case 0x95:
-    return 'O'; // Ó Ô Õ
-  case 0x9A:
-  case 0x9C:
-    return 'U'; // Ú Ü
-  case 0x87:
-    return 'C'; // Ç
+    case 0xA1:
+    case 0xA0:
+    case 0xA2:
+    case 0xA3:
+      return 'a'; // á à â ã
+    case 0xA9:
+    case 0xAA:
+      return 'e'; // é ê
+    case 0xAD:
+      return 'i'; // í
+    case 0xB3:
+    case 0xB4:
+    case 0xB5:
+      return 'o'; // ó ô õ
+    case 0xBA:
+    case 0xBC:
+      return 'u'; // ú ü
+    case 0xA7:
+      return 'c'; // ç
+    case 0x81:
+    case 0x80:
+    case 0x82:
+    case 0x83:
+      return 'A'; // Á À Â Ã
+    case 0x89:
+    case 0x8A:
+      return 'E'; // É Ê
+    case 0x8D:
+      return 'I'; // Í
+    case 0x93:
+    case 0x94:
+    case 0x95:
+      return 'O'; // Ó Ô Õ
+    case 0x9A:
+    case 0x9C:
+      return 'U'; // Ú Ü
+    case 0x87:
+      return 'C'; // Ç
   }
   return 0;
 }
